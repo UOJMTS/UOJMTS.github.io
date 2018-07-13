01c313afb51e0dfe16a9fde4a2c592e6be0b2ee4
UOJMTS <40804775+UOJMTS@users.noreply.github.com>
2018-07-13 02:04:59 -0400

    part1

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,6 +21,18 @@ function hideVideo(e) {
 }
 
 
+//video1
+
+var figure = $(".video1").hover( hoverVideo, hideVideo );
+
+function hoverVideo(e) {  
+    $('video', this).get(0).play(); 
+}
+
+function hideVideo(e) {
+    $('video', this).get(0).pause(); 
+}
+
 	// Breakpoints.
 		breakpoints({
 			default:   ['1681px',   null       ],
